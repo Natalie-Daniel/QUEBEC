026aff1b5176bcdaa9cf32af5d3ab59d83c9e8ac
Brown <nbrown7@una.edu>
2023-02-14 16:45:07 -0600

    saving code from class

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000;
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nbrown7:UNAlions1122@cluster0.x48nd31.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log("before connection;")
+
+client.connect(err => {
+  console.log("in connect method");
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World! Frosty Here!')
 })
